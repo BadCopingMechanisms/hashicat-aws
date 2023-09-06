5259e0083df5517a4f7f382a45683b9c8e87c503
BadCopingMechanisms <y.noret@gmail.com>
2023-09-06 22:25:08 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/chip_company/s3-bucket/aws"
   version = "2.8.0"
   
-  bucket = "my-guhydsufgsufygsufgsufgusfb""
+  bucket = "my-guhydsufgsufb"
   acl    = "private"
 
   versioning = {
